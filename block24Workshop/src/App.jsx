675db901bc7fcfd2b96150514fa1a95a0fd63e76
Jordan <jordanharris1833@yahoo.com>
2024-08-19 13:57:31 -0500

    styled page finished up code

--- block24Workshop/src/App.jsx
+++ block24Workshop/src/App.jsx
@@ -5,14 +5,34 @@ import { puppyList } from "./data.js";
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
 
   console.log("Puppy List:", puppies);
 
+  const featuredPup = puppies.find((pup) => pup.id === featPupId);
+  console.log(featuredPup);
+
   return (
     <>
+      {featPupId && (
+        <div className="card">
+          <h2 className="featPup">{featuredPup.name}</h2>
+          <ul className="featList">
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+      <br />
+      <br />
       <div className="App">
+        <h3>Select a Puppy Below:</h3>
         {puppies.map((puppy) => {
-          return <p key={puppy.id}>{puppy.name}</p>;
+          return (
+            <button onClick={() => setFeatPupId(puppy.id)} key={puppy.id}>
+              {puppy.name}
+            </button>
+          );
         })}
       </div>
     </>
